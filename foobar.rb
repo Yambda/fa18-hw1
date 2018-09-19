920a5cde9c2e95b54cdffe35dd5426719a8a9bda
Edward Yam <edwardyam@berkeley.edu>
2018-09-18 22:27:33 -0700

    submitting hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,23 @@ class Foobar
   def self.baz(a)
     # Class method, not an instance method
     # Call with `Foobar.baz`
-    # Q4 CODE HERE
+    # Q4 CODE HERE]
+    a = a.sort.map{|i| i.to_i}
+    if a[0] < 8 and a[0]%2 == 0
+        sum = a[0] + 2
+    else
+        sum = 0
+    end
+    current = a[0]
+    for i in 0..(a.length - 1)
+        number = a[i] + 2
+        if a[i] != current
+            current = a[i]
+            if number%2 == 0 and number < 10
+                sum += number
+            end
+        end
+    end
+    return sum
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,13 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+  return (a + b)**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  return a.sort.map{|i| i + 1}
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -21,32 +18,32 @@ end
 
 def scrabble(word)
   values = {
-    a: 1,
-    b: 3,
-    c: 3,
-    d: 2,
-    e: 1,
-    f: 4,
-    g: 2,
-    h: 4,
-    i: 1,
-    j: 8,
-    k: 5,
-    l: 1,
-    m: 3,
-    n: 1,
-    o: 1,
-    p: 3,
-    q: 10,
-    r: 1,
-    s: 1,
-    t: 1,
-    u: 1,
-    v: 4,
-    w: 4,
-    x: 8,
-    y: 4,
-    z: 10,
+    "a" => 1,
+    "b" => 3,
+    "c" => 3,
+    "d" => 2,
+    "e" => 1,
+    "f" => 4,
+    "g" => 2,
+    "h" => 4,
+    "i" => 1,
+    "j" => 8,
+    "k" => 5,
+    "l" => 1,
+    "m" => 3,
+    "n" => 1,
+    "o" => 1,
+    "p" => 3,
+    "q" => 10,
+    "r" => 1,
+    "s" => 1,
+    "t" => 1,
+    "u" => 1,
+    "v" => 4,
+    "w" => 4,
+    "x" => 8,
+    "y" => 4,
+    "z" => 10,
   }
-  # Q5 CODE HERE
+  return word.split("").map { |e| values[e] }.sum
 end
